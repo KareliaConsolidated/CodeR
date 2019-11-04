b4adb931889d419c60811d5535030d010435595c
Anubhav Karelia <kareliaanubhav@outlook.com>
2019-11-04 17:25:55 +0530

    Introduction to Basics

---
+++ Datacamp/01_Introduction_to_R/01_Introduction_to_Basics.R
@@ -0,0 +1,84 @@
+# An addition
+5 + 5 
+
+# A subtraction
+5 - 5 
+
+# A multiplication
+3 * 5
+
+# A division
+(5 + 5) / 2 
+
+# Exponentiation
+2 ^ 5
+
+# Modulo
+28 %% 6
+
+# ===============Variable Assignment================#
+
+# Assign the value 42 to x
+x <- 42
+
+# Print out the value of the variable x
+x
+
+# Assign the value 5 to the variable my_apples
+my_apples <- 5
+
+# Print out the value of the variable my_apples
+my_apples
+
+# Assign a value to the variables my_apples and my_oranges
+my_apples <- 5
+my_oranges <- 6
+
+# Add these two variables together
+my_apples + my_oranges
+
+# Create the variable my_fruit
+my_fruit <- my_apples + my_oranges
+
+# Assign a value to the variable my_apples
+my_apples <- 5 
+
+# Fix the assignment of my_oranges
+my_oranges <- "six" 
+
+# Create the variable my_fruit and print it out
+# my_fruit <- my_apples + my_oranges  # Error: non-numeric argument to binary operator
+
+my_fruit
+
+#==================Basic Data Types in R==================#
+# R works with numerous data types. Some of the most basic types to get started are:
+#   
+# Decimal values like 4.5 are called numerics.
+# Natural numbers like 4 are called integers. Integers are also numerics.
+# Boolean values (TRUE or FALSE) are called logical.
+# Text (or string) values are called characters.
+
+# Change my_numeric to be 42
+my_numeric <- 42
+
+# Change my_character to be "universe"
+my_character <- "universe"
+
+# Change my_logical to be FALSE
+my_logical <- FALSE
+
+#=================To Check Data Types=========================#
+# Declare variables of different types
+my_numeric <- 42
+my_character <- "universe"
+my_logical <- FALSE 
+
+# Check class of my_numeric
+class(my_numeric)
+
+# Check class of my_character
+class(my_character)
+
+# Check class of my_logical
+class(my_logical)
